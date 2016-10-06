d88ced435e9784899eb4a97fdc812c6ebdf6d727
Liulijin <llj098@gmail.com>
2016-10-06 14:37:32 +0800

    first

---
+++ app/src/main/java/demo/interrcs/patch/App.java
@@ -0,0 +1,15 @@
+package demo.interrcs.patch;
+
+import com.tencent.tinker.loader.app.TinkerApplication;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+/**
+ * Created by lj on 06/10/2016.
+ */
+
+public class App extends TinkerApplication {
+    public App() {
+        super(ShareConstants.TINKER_ENABLE_ALL);
+
+    }
+}
