bca3d505959f00c5e5f14968d4dd489180bd381c
Liulijin <llj098@gmail.com>
2016-10-06 15:44:11 +0800

    add downloading

--- Makefile
+++ Makefile
@@ -9,3 +9,9 @@ patch:
 	gradle clean
 	gradle tinkerPatchDebug -POLD_APK=./apk/old.apk -PAPPLY_RESOURCE=./apk/old-R.txt
 	mv app/build/outputs/tinkerPatch/debug/patch_signed_7zip.apk apk/patch.apk
+
+sd:
+	adb push apk/patch.apk /sdcard/
+
+deploy:
+	scp apk/patch.apk root@gfw.rongju.im:tmp/
--- app/src/main/java/demo/interrcs/patch/MainActivity.java
+++ app/src/main/java/demo/interrcs/patch/MainActivity.java
@@ -4,19 +4,34 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.tencent.tinker.lib.tinker.Tinker;
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okio.BufferedSink;
+import okio.Okio;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainAct";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button)findViewById(R.id.button);
+        Button btn = (Button) findViewById(R.id.button);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +40,59 @@ public class MainActivity extends AppCompatActivity {
                         Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch.apk");
             }
         });
+
+        Button btnClean = (Button) findViewById(R.id.btn_clean);
+        btnClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Tinker.with(getApplicationContext()).cleanPatch();
+            }
+        });
+
+        ((TextView)findViewById(R.id.textView)).setVisibility(View.INVISIBLE);
+
+        if (Tinker.with(getApplicationContext()).isTinkerLoaded()) {
+            return;
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "try downloading..", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder().url("http://gfw.rongju.im:8888/patch.apk").build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    Log.e(TAG, "response is succ:" + response.isSuccessful());
+                    File downloadedFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+                    BufferedSink sink = Okio.buffer(Okio.sink(downloadedFile));
+                    sink.writeAll(response.body().source());
+                    sink.close();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "patch 下载成功!", Toast.LENGTH_LONG).show();
+                            TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+                        }
+                    });
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        }).start();
+
     }
+
 }
